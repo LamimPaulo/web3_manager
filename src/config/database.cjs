d7d1b88c4b2adea60499604af00c1767454722c9
Paulo <paulo@MacBook-Pro-de-Paulo.local>
2022-01-06 23:23:55 -0300

    database config from env

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,9 +1,10 @@
+require('dotenv').config();
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'evm-offscreen',
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
     define: {
       timestamps: true,
       underscored: true,

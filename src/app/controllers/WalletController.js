db82b24dc8c2438e5c57293e2e631ee3ef74b4dc
Paulo <paulo@MacBook-Pro-de-Paulo.local>
2022-03-07 17:12:42 -0300

    alter testnet

--- src/app/controllers/WalletController.js
+++ src/app/controllers/WalletController.js
@@ -15,7 +15,6 @@ class WalletController {
 
     async getInTransactions(address, abbr) {
         var contract_address = await this.getContract(abbr)
-        var contract_abi = await this.getABI(abbr)
 
         // const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc&apikey=8Z5ZEBBPEIFWWJA54XM24IQNSXZFC8ZDUS', {
         const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc', {
@@ -29,7 +28,7 @@ class WalletController {
 
         res.result.forEach(r => {
             console.log(r.value)
-            if(r.value > 0 && r.to == address.toLowerCase() && r.contractAddress == contract_address){
+            // if(r.value > 0 && r.to == address.toLowerCase() && r.contractAddress == contract_address){
                 const w3 = new Web3(process.env.PROVIDER_URL);
                 r.value = w3.utils.fromWei(r.value)
                 r.gasPrice = w3.utils.fromWei(r.gasPrice)
@@ -37,7 +36,7 @@ class WalletController {
                 r.gas = w3.utils.fromWei(r.gas)
                 r.cumulativeGasUsed = w3.utils.fromWei(r.cumulativeGasUsed)
                 filtered.push(r)
-            }
+            // }
         });
 
         return filtered
@@ -146,7 +145,6 @@ class WalletController {
 
             return ABI
     }
-
 }
 
 export default WalletController;

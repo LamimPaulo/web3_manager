e45eca7b2ba47bf7eee96f6406ec8b291c47369f
Paulo <paulo@MacBook-Pro-de-Paulo.local>
2022-03-08 16:23:28 -0300

    dump sync wallets

--- src/app/controllers/WalletController.js
+++ src/app/controllers/WalletController.js
@@ -23,12 +23,11 @@ class WalletController {
             });
 
         const res = await response.json();
-return res.result;
         var filtered = []
 
         res.result.forEach(r => {
             console.log(r.value)
-            if(r.value > 0 && r.to.toLowerCase() == address.toLowerCase() && r.contractAddress == contract_address){
+            if(r.value > 0 && r.to.toLowerCase() == address.toLowerCase() && r.contractAddress.toLowerCase() == contract_address.toLowerCase()){
                 r.value = w3.utils.fromWei(r.value)
                 const w3 = new Web3(process.env.PROVIDER_URL);
                 r.gasPrice = w3.utils.fromWei(r.gasPrice)

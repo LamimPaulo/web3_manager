32d96bb7ef9320db76c83bb252ea9770fbe734ac
Paulo Lamim <Robertolamim@gmail.com>
2022-06-02 10:33:34 -0300

    error hotfix

--- src/app/controllers/WalletController.js
+++ src/app/controllers/WalletController.js
@@ -16,8 +16,8 @@ class WalletController {
     async getInTransactions(address, abbr) {
         var contract_address = await this.getContract(abbr)
 
-        // const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc&apikey=8Z5ZEBBPEIFWWJA54XM24IQNSXZFC8ZDUS', {
-        const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc', {
+        const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc&apikey=8Z5ZEBBPEIFWWJA54XM24IQNSXZFC8ZDUS', {
+        // const response = await fetch(process.env.BSC_ADDRESS+'api?module=account&action=tokentx&='+contract_address+'&address='+address+'&page=1&offset=0&startblock=0&endblock=999999999&sort=desc', {
             method: 'get',
             headers: {'Content-Type': 'application/json'}
             });

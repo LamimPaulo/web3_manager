1289f554ce203a10c4bc5521772b8e16b0ddcadb
Paulo Lamim <Robertolamim@gmail.com>
2022-09-26 20:10:06 -0300

    fix token notify

--- src/app/models/SystemWallet.js
+++ src/app/models/SystemWallet.js
@@ -1,9 +1,11 @@
 import Sequelize, { Model } from "sequelize";
+import { INTEGER } from "sequelize";
  
 class SystemWallet extends Model {
   static init(sequelize) {
     super.init(
       {
+        id: Sequelize.INTEGER,
         name: Sequelize.STRING(),
         address: Sequelize.STRING(100),
         priv: Sequelize.STRING(100),

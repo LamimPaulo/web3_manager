aba4b714925db95ffdafc90d9ee5abbd5d89908a
Paulo Lamim <robertolamim@gmail.com>
2024-02-09 11:21:04 -0500

    debug

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -799,18 +799,22 @@ class TransactionController {
         const contractData = await contractIstance.encodeABI();
         console.log("3 indo... ");
 
-        const estimatedGas = await contractIstance.estimateGas(
-            {
-                from: address,
-                gasPrice: web3.eth.gas_price,
-                data: contractData
+        // const estimatedGas = await contractIstance.estimateGas(
+        //     {
+        //         from: address,
+        //         gasPrice: web3.eth.gas_price,
+        //         data: contractData
 
-            }, function(error, estimatedGas) {
-                console.log('estimate error',error, estimatedGas);
-            }
-        );
+        //     }, function(error, estimatedGas) {
+        //         console.log('estimate error',error, estimatedGas);
+        //     }
+        // );
 
-        console.log("estimatedGas: ", estimatedGas)
+        // console.log("estimatedGas: ", estimatedGas)
+
+        console.log("4 indo... ");
+
+
 
         const rawTransaction = {
             from: master.address,
